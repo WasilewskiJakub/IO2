b8b461972d4c6aabc6a05684aef0c0cd4af2a49f
Jakub Wasilewski <98490230+WasilewskiJakub@users.noreply.github.com>
2023-03-05 21:52:06 +0100

    Program

---
+++ Lab2IO2/StringCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2IO2
+{
+    public class StringCalculator
+    {
+        public static int SumString(string str)
+        {
+            if (str == String.Empty)
+                return 0;
+
+            int result = 0;
+            if (int.TryParse(str, out result))
+            {
+                if (result < 0)
+                    throw new ArgumentException();
+                if (result > 1000)
+                    return 0;
+                return result;
+            }
+            var numbers = str.Split(',', '\n');
+            foreach (var number in numbers)
+            {
+                int num = int.Parse(number);
+                if (num < 0) throw new ArgumentException();
+                if (num > 1000) num = 0;
+                result += num;
+            }
+            return result;
+        }
+    }
+}
